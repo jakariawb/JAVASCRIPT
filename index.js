fd4d24a37e40b8c96dc29608b279feba922424c3
jakariawb <166798315+jakariawb@users.noreply.github.com>
2025-01-24 13:21:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+// document.write('Html<br/>');
+// document.write('css<br/>');
+// document.write('tailwind css <br/>')
+// document.write('Dejiy ui')
+
+// console.log('Hello')
+
+// var number1 = 10;
+// var number2 = 20;
+
+// document.write ('number1 = '+ number1 + ' and  number2 = ' +number2)
+
+// var name = (' jakaria')
+// var nmb = 12;
+// var nameList = ('jakaria', 'abir', 'numane', 'tinver','mahide' )
+// document.write(name + nmb + " I am forme canada " + nameList)
+
+// var me = 2;
+
+// while(me <= 10){
+//     document.write('looooooooooping',me);
+//     me++;
+// }
+// document.write("list number and odod naumber",me)
+//prompt massage/////
+// var name = prompt('Enter your name:')
+// var firstName = ('Md');
+// var lastName = ('  JAKARIA');
+// var text = ('full name : ');
+// text = text.toUpperCase();
+
+// document.write(text)
+// document.write(firstName + lastName + firstName.length + " <br/>");
---
+++ user-input.js
@@ -0,0 +1,18 @@
+var num1 = prompt ('Enter Fist Number : ');
+var num2 = prompt ('Enter Cecond Number :');
+
+num1 = parseInt(num1, 10);
+
+num2 = parseInt(num2, 10);
+   
+
+ var sum , sub;
+
+ sum = num1 + num2;
+
+
+document.write('addition = ' + sum + '<br/>');
+
+sum = num1  - num2;
+
+document.write('subtraction = ' + sum)
