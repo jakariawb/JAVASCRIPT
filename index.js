d292d4ce92f398e190020df1bba7bdcf410d28ba
jakariawb <166798315+jakariawb@users.noreply.github.com>
2025-01-24 13:27:25 +0600

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
-// document.write('Html<br/>');
-// document.write('css<br/>');
-// document.write('tailwind css <br/>')
-// document.write('Dejiy ui')
+document.write('Html<br/>');
+document.write('css<br/>');
+document.write('tailwind css <br/>')
+document.write('Dejiy ui')
 
-// console.log('Hello')
+console.log('Hello')
 
-// var number1 = 10;
-// var number2 = 20;
+var number1 = 10;
+var number2 = 20;
 
-// document.write ('number1 = '+ number1 + ' and  number2 = ' +number2)
+document.write ('number1 = '+ number1 + ' and  number2 = ' +number2)
 
-// var name = (' jakaria')
-// var nmb = 12;
-// var nameList = ('jakaria', 'abir', 'numane', 'tinver','mahide' )
-// document.write(name + nmb + " I am forme canada " + nameList)
+var name = (' jakaria')
+var nmb = 12;
+var nameList = ('jakaria', 'abir', 'numane', 'tinver','mahide' )
+document.write(name + nmb + " I am forme canada " + nameList)
 
 // var me = 2;
 
@@ -22,13 +22,13 @@
 //     document.write('looooooooooping',me);
 //     me++;
 // }
-// document.write("list number and odod naumber",me)
-//prompt massage/////
-// var name = prompt('Enter your name:')
-// var firstName = ('Md');
-// var lastName = ('  JAKARIA');
-// var text = ('full name : ');
-// text = text.toUpperCase();
+document.write("list number and odod naumber",me)
+// prompt massage/////
+var name = prompt('Enter your name:')
+var firstName = ('Md');
+var lastName = ('  JAKARIA');
+var text = ('full name : ');
+text = text.toUpperCase();
 
-// document.write(text)
-// document.write(firstName + lastName + firstName.length + " <br/>");
+document.write(text)
+document.write(firstName + lastName + firstName.length + " <br/>");
