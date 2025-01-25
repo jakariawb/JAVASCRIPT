b29f7754652f49a17af887e08e84fd56560f0399
jakariawb <166798315+jakariawb@users.noreply.github.com>
2025-01-25 13:19:51 +0600

    first commit

---
+++ task.3.js
@@ -0,0 +1,43 @@
+var num1 = prompt ('Enter Fist Number :');
+var num2 = prompt('Enter Cecond Number :');
+
+
+num1 = parseInt (num1, 20);
+num2 = parseInt (num2,10);
+
+var sum , sub;
+sum = num1 + num2;
+
+document.write('Enter Fist Number = '+ sum + '<br/>');
+document.write('Enter Cecond Number =' + sum + '<br/>');
+
+document.write('20 + 10 = '+ sum + '<br/>')
+
+sum = num1 - num2;
+
+document.write('20 - 10 =' + sum + '<br/>')
+
+var num1 = prompt ('Enter Fist Number :');
+var num2 = prompt('Enter Cecond Number :');
+
+var num1 = prompt ('Enter Fist Number :');
+var num2 = prompt('Enter Cecond Number :');
+
+num1 = parseInt (num1, 20);
+num2 = parseInt (num2, 10);
+
+
+
+num1 = parseInt (num1, 20);
+num2 = parseInt (num2, 10);
+
+sum = num1 / num2;
+
+document.write('20 / 10 = ' + sum)
+
+sum = num1 % num2;
+
+document.write('20 % 10 = ' + sum + '<br/>')
+sum = num1 * num2;
+
+document.write('20 * 10 = ' + sum)
