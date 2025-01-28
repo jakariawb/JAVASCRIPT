22027f08a313f9db37c07675bcb2b40ee68d8956
jakariawb <166798315+jakariawb@users.noreply.github.com>
2025-01-28 11:57:29 +0600

    first commit

--- logk.js
+++ logk.js
@@ -2,11 +2,15 @@ var num1 = prompt('Enter num1 :');
 var num2 = prompt('Enter num2 :');
 var num3 = prompt('Enter num3 :');
 
-if(num1 > num2 && num1 > num3)
-    console.log('large number1 =' + num1);
+if(num1 > num2 && num1 > num3){
 
-else if(num2 > num1 && num2 > num3)
+    console.log('large number1 =' + num1);
+}
+
+else if(num2 > num1 && num2 > num3){
+
     console.log('large number2 ='+ num2)
+}
 
 else if(num3 > num1 && num3 > num2){
 
