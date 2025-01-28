2142eb4d68e4c6c018d6ca9ffe06aef3154605cb
jakariawb <166798315+jakariawb@users.noreply.github.com>
2025-01-28 12:22:30 +0600

    first commit

--- logk.js
+++ logk.js
@@ -3,22 +3,27 @@ var num2 = prompt('Enter num2 :');
 var num3 = prompt('Enter num3 :');
 
 if(num1 > num2 && num1 > num3){
-
-    console.log('large number1 =' + num1);
+   console.log('large number1 =' + num1);
 }
 
 else if(num2 > num1 && num2 > num3){
-
     console.log('large number2 ='+ num2)
 }
 
 else if(num3 > num1 && num3 > num2){
-
     console.log('larg number3 =' + num3);
-
-}
-    
+}   
 else{
     console.log('no number')
 }
   
+var letter = prompt('Enter a letter :');
+
+letter = letter.toLowerCase();
+
+if (letter == 'a'|| letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+    console.log('vowel')
+}
+else{
+    console.log('consonant')
+}
